10e0fcf36c558c6c35c7da54af0281d4478bcdc0
Jared <proulxj1@outlook.com>
2015-03-05 12:49:53 -0500

    Update 2 (In the middle of working)
    Stuff was done. Lots of versions were made.

--- ICalculatorApplication/Program.cs
+++ ICalculatorApplication/Program.cs
@@ -10,6 +10,32 @@ namespace ICalculatorApplication
     {
         static void Main(string[] args)
         {
-        }
+            List<int> numbers;
+            string info;
+
+            for (int i = 0; i < 20; i++ )
+            {
+                info = Console.ReadLine();
+                while (true)
+                {
+                    try
+                    {
+                        int numberSS = Int32.Parse(info);
+                        Console.Write(info);
+                        i++;
+                        break;
+                    }
+
+                    catch (InvalidCastException e)
+                    {
+                        Console.WriteLine(e.Message+ "Do you wish to continue? Y/N?");
+                        
+                        Console.ReadKey();
+
+                    }
+                }
+            }
+
+        }     
     }
 }
